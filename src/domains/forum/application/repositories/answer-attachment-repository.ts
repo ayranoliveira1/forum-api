ef32d2c12fb6f4f39c939366e40375887b23e2cd
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-27 18:30:25 -0300

    feat: transform repository interfaces into abstract classes

--- src/domains/forum/application/repositories/answer-attachment-repository.ts
+++ src/domains/forum/application/repositories/answer-attachment-repository.ts
@@ -1,6 +1,6 @@
 import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
 
-export interface AnswerAttchmentsRepository {
-  findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]>
-  deleteManyByAnswerId(answerId: string): Promise<void>
+export abstract class AnswerAttchmentsRepository {
+  abstract findManyByAnswerId(answerId: string): Promise<AnswerAttachment[]>
+  abstract deleteManyByAnswerId(answerId: string): Promise<void>
 }
--- src/domains/forum/application/repositories/answer-comments-repository.ts
+++ src/domains/forum/application/repositories/answer-comments-repository.ts
@@ -1,12 +1,12 @@
 import { PaginationParms } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comments'
 
-export interface AnswerCommentsRepository {
-  findById(id: string): Promise<AnswerComment | null>
-  findManyByAnswerId(
+export abstract class AnswerCommentsRepository {
+  abstract findById(id: string): Promise<AnswerComment | null>
+  abstract findManyByAnswerId(
     answerId: string,
     params: PaginationParms,
   ): Promise<AnswerComment[]>
-  create(answerComment: AnswerComment): Promise<void>
-  delete(answerComment: AnswerComment): Promise<void>
+  abstract create(answerComment: AnswerComment): Promise<void>
+  abstract delete(answerComment: AnswerComment): Promise<void>
 }
