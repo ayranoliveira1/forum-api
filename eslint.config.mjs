906a8da152f4aaa6292bf0a8effcc316dbc9621b
ayranoliveira1 <ayranoliveira1@gmail.com>
2025-03-27 11:53:09 -0300

    chore: add dayjs and faker setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,9 +20,9 @@ export default defineConfig([
     rules: {
       quotes: ['error', 'single'],
       semi: ['error', 'never'],
-      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      // '@typescript-eslint/no-empty-object-type': 'off',
       // '@typescript-eslint/no-unused-vars': 'off',
-      // "@typescript-eslint/no-explicit-any": "off",
       // '@typescript-eslint/no-empty-interface': 'off',
       // '@typescript-eslint/ban-types': 'off',
     },
